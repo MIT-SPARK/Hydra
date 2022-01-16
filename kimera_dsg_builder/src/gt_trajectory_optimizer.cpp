85d9f0dac7a87adadb93b51bd719680f4c9306ac
Nathan Hughes <na26933@mit.edu>
2022-01-16 23:37:28 +0000

    optionally use valence factors for gt optimizer, sidpac 3-4 config updates

--- kimera_dsg_builder/src/gt_trajectory_optimizer.cpp
+++ kimera_dsg_builder/src/gt_trajectory_optimizer.cpp
@@ -1,9 +1,11 @@
 #include <KimeraRPGO/RobustSolver.h>
 #include <gtsam/inference/Symbol.h>
 #include <gtsam/nonlinear/GncOptimizer.h>
+#include <gtsam/sam/RangeFactor.h>
 #include <gtsam/slam/BoundingConstraint.h>
 #include <gtsam/slam/dataset.h>
 #include <kimera_dsg/dynamic_scene_graph.h>
+#include <kimera_pgmo/DeformationGraph.h>
 #include <ros/package.h>
 
 #include <gflags/gflags.h>
@@ -12,14 +14,17 @@
 
 DEFINE_string(result_dir, "", "directory to read from");
 DEFINE_string(g2o_file, "pgmo/result.g2o", "file to read");
+DEFINE_string(pgmo_file, "pgmo/deformation_graph.dgrf", "deformation graph");
 DEFINE_string(dsg_file, "backend/dsg.json", "file to read");
 DEFINE_string(config_file, "gt_sidpac_f34.yaml", "file to read");
 DEFINE_string(agent_prefix, "a", "agent prefix");
+DEFINE_bool(use_g2o, false, "use g2o file");
 
 using namespace KimeraRPGO;
 using namespace kimera;
 using PoseFactor = gtsam::BetweenFactor<gtsam::Pose3>;
 using PriorFactor = gtsam::PriorFactor<gtsam::Pose3>;
+using ManualLCFactor = gtsam::RangeFactor<gtsam::Pose3, gtsam::Pose3, double>;
 
 class Pose3Bounds : public gtsam::BoundingConstraint1<gtsam::Pose3> {
  public:
@@ -82,7 +87,8 @@ void add_height(gtsam::NonlinearFactorGraph& factors,
                 size_t end,
                 size_t skip,
                 double height,
-                double variance = 1e-1) {
+                double variance = 1e-1,
+                double other_variance = 100.0) {
   for (size_t i = start; i < end; i += skip) {
     const auto key = gtsam::Symbol(FLAGS_agent_prefix[0], i);
     if (!values.exists(key)) {
@@ -95,7 +101,8 @@ void add_height(gtsam::NonlinearFactorGraph& factors,
         orig_pose.translation().x(), orig_pose.translation().y(), height);
 
     Eigen::VectorXd vars(6);
-    vars << 100, 100, 100, 100, 100, variance;
+    vars << other_variance, other_variance, other_variance, other_variance,
+        other_variance, variance;
     auto noise = gtsam::noiseModel::Diagonal::Variances(vars);
 
     factors.add(PriorFactor(key, gtsam::Pose3(prior_rot, prior_trans), noise));
@@ -117,6 +124,185 @@ std::vector<size_t> read_timestamps(const std::string& filepath) {
   return times_ns;
 }
 
+void load_factors_g2o(gtsam::NonlinearFactorGraph& factors, gtsam::Values& values) {
+  const std::string g2o_file = FLAGS_result_dir + "/" + FLAGS_g2o_file;
+  auto g2o_info = gtsam::readG2o(g2o_file, true);
+  if (!g2o_info.first || g2o_info.first->size() == 0) {
+    LOG(FATAL) << "empty g2o file!";
+    return;
+  }
+
+  factors = *g2o_info.first;
+  values = *g2o_info.second;
+  const char agent_prefix = FLAGS_agent_prefix[0];
+
+  std::vector<gtsam::Key> to_erase;
+  for (const auto& key : values.keys()) {
+    if (gtsam::Symbol(key).chr() != agent_prefix) {
+      to_erase.push_back(key);
+    }
+  }
+
+  for (const auto& key : to_erase) {
+    values.erase(key);
+  }
+}
+
+void add_priors_from_config(const YAML::Node& config,
+                            gtsam::Values& values,
+                            gtsam::NonlinearFactorGraph& factors) {
+  for (const auto& constraint : config["heights"]) {
+    size_t start = constraint["start"].as<size_t>();
+    size_t stop = constraint["stop"].as<size_t>();
+    size_t inc = constraint["inc"].as<size_t>();
+    double height = constraint["height"].as<double>();
+    double variance = constraint["variance"].as<double>();
+    double default_variance = constraint["default_variance"].as<double>();
+    LOG(INFO) << constraint;
+    LOG(INFO) << "Adding height prior: range(" << start << ", " << stop << ", " << inc
+              << ") @ " << height << ", var=" << variance
+              << " (default=" << default_variance << ")";
+    add_height(factors, values, start, stop, inc, height, variance, default_variance);
+  }
+}
+
+void add_bounds_from_config(const YAML::Node& config,
+                            gtsam::Values& values,
+                            gtsam::NonlinearFactorGraph& factors) {
+  for (const auto& constraint : config["height_bounds"]) {
+    size_t start = constraint["start"].as<size_t>();
+    size_t stop = constraint["stop"].as<size_t>();
+    size_t inc = constraint["inc"].as<size_t>();
+    double lower = constraint["lower"].as<double>();
+    double upper = constraint["upper"].as<double>();
+    double mu = constraint["mu"].as<double>();
+    LOG(INFO) << "Adding height constraint: range(" << start << ", " << stop << ", "
+              << inc << ") -> [" << lower << ", " << upper << "], mu=" << mu;
+    add_height_bounds(factors, values, start, stop, inc, lower, upper, mu);
+  }
+}
+
+void output(const gtsam::NonlinearFactorGraph& factors, gtsam::Values& values) {
+  const char agent_prefix = FLAGS_agent_prefix[0];
+
+  std::vector<gtsam::Key> to_erase;
+  for (const auto& key : values.keys()) {
+    if (gtsam::Symbol(key).chr() != agent_prefix) {
+      to_erase.push_back(key);
+    }
+  }
+
+  for (const auto& key : to_erase) {
+    values.erase(key);
+  }
+
+  gtsam::writeG2o(factors, values, "opt_trajectory.g2o");
+
+  std::string dsg_file = FLAGS_result_dir + "/" + FLAGS_dsg_file;
+  auto timestamps = read_timestamps(dsg_file);
+
+  std::ofstream fout("opt_trajectory.csv");
+  fout << "#timestamp_kf,x,y,z,qw,qx,qy,qz" << std::endl;
+
+  for (const auto& kv_pair : values) {
+    gtsam::Symbol symb(kv_pair.key);
+    if (symb.chr() != agent_prefix) {
+      continue;
+    }
+
+    if (symb.index() >= timestamps.size()) {
+      LOG(FATAL) << "timestamps and values disagree: " << symb.index()
+                 << " >= " << timestamps.size();
+    }
+
+    fout << timestamps[symb.index()] << ",";
+
+    auto pose = kv_pair.value.cast<gtsam::Pose3>();
+    Eigen::Vector3d t = pose.translation();
+    auto q = pose.rotation().toQuaternion();
+
+    fout << t.x() << "," << t.y() << "," << t.z() << "," << q.w() << "," << q.x() << ","
+         << q.y() << "," << q.z() << std::endl;
+  }
+}
+
+void load_factors_pgmo(const RobustSolverParams& params,
+                       gtsam::NonlinearFactorGraph& factors,
+                       gtsam::Values& values) {
+  const std::string pgmo_file = FLAGS_result_dir + "/" + FLAGS_pgmo_file;
+
+  kimera_pgmo::DeformationGraph graph;
+  graph.initialize(params);
+  graph.load(pgmo_file);
+
+  factors = graph.getGtsamFactors();
+  values = graph.getGtsamValues();
+}
+
+void add_manual_loop_clousres(const YAML::Node& config,
+                              gtsam::NonlinearFactorGraph& factors) {
+  const double var = config["manual_lc_var"].as<double>();
+
+  Eigen::VectorXd lc_vars(1);
+  lc_vars(0) = var;
+  auto lc_noise = gtsam::noiseModel::Diagonal::Variances(lc_vars);
+
+  const char agent_prefix = FLAGS_agent_prefix[0];
+  for (const auto& factor_info : config["manual_loop_closures"]) {
+    gtsam::Symbol key1(agent_prefix, factor_info["index1"].as<size_t>());
+    gtsam::Symbol key2(agent_prefix, factor_info["index2"].as<size_t>());
+    LOG(INFO) << "Adding manual loop closure: " << key1 << " -> " << key2;
+    factors.emplace_shared<ManualLCFactor>(key1, key2, 0.0, lc_noise);
+  }
+}
+
+void remap_covariances(const YAML::Node& config,
+                       const gtsam::NonlinearFactorGraph& old_factors,
+                       gtsam::NonlinearFactorGraph& factors) {
+  const double odom_rot_var = config["odom_rot"].as<double>();
+  const double odom_trans_var = config["odom_trans"].as<double>();
+  const double lc_rot_var = config["loop_close_rot"].as<double>();
+  const double lc_trans_var = config["loop_close_trans"].as<double>();
+
+  Eigen::VectorXd odom_vars(6);
+  odom_vars << odom_rot_var, odom_rot_var, odom_rot_var, odom_trans_var, odom_trans_var,
+      odom_trans_var;
+  auto odom_noise = gtsam::noiseModel::Diagonal::Variances(odom_vars);
+
+  Eigen::VectorXd lc_vars(6);
+  lc_vars << lc_rot_var, lc_rot_var, lc_rot_var, lc_trans_var, lc_trans_var,
+      lc_trans_var;
+  auto lc_noise = gtsam::noiseModel::Diagonal::Variances(lc_vars);
+
+  const char agent_prefix = FLAGS_agent_prefix[0];
+  for (const auto& base_factor : old_factors) {
+    if (base_factor->keys().size() != 1) {
+      factors.push_back(base_factor);
+      continue;
+    }
+
+    const gtsam::Symbol symb1(base_factor->keys()[0]);
+    const gtsam::Symbol symb2(base_factor->keys()[1]);
+    if (symb1.chr() != agent_prefix || symb2.chr() != agent_prefix) {
+      factors.push_back(base_factor);
+      continue;
+    }
+
+    const auto& factor = dynamic_cast<const PoseFactor&>(*base_factor);
+
+    if (std::abs(static_cast<int64_t>(symb1.index()) -
+                 static_cast<int64_t>(symb2.index())) > 1) {
+      LOG(INFO) << "Loop closure: " << gtsam::Symbol(factor.keys()[0]) << " -> "
+                << gtsam::Symbol(factor.keys()[1]);
+      factors.emplace_shared<PoseFactor>(
+          factor.keys()[0], factor.keys()[1], factor.measured(), lc_noise);
+    } else {
+      factors.emplace_shared<PoseFactor>(
+          factor.keys()[0], factor.keys()[1], factor.measured(), odom_noise);
+    }
+  }
+}
+
 int main(int argc, char* argv[]) {
   FLAGS_minloglevel = 0;
   FLAGS_logtostderr = 1;
@@ -132,83 +318,10 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
-  const std::string g2o_file = FLAGS_result_dir + "/" + FLAGS_g2o_file;
-  auto g2o_info = gtsam::readG2o(g2o_file, true);
-  if (!g2o_info.first || g2o_info.first->size() == 0) {
-    LOG(FATAL) << "empty g2o file!";
-    return 1;
-  }
-
   std::string config_path =
       ros::package::getPath("kimera_dsg_builder") + "/config/" + FLAGS_config_file;
   YAML::Node config = YAML::LoadFile(config_path);
 
-  auto old_factors = *g2o_info.first;
-  auto values = *g2o_info.second;
-  const char agent_prefix = FLAGS_agent_prefix[0];
-
-  std::vector<gtsam::Key> to_erase;
-  for (const auto& key : values.keys()) {
-    if (gtsam::Symbol(key).chr() != agent_prefix) {
-      to_erase.push_back(key);
-    }
-  }
-
-  for (const auto& key : to_erase) {
-    values.erase(key);
-  }
-
-  const double odom_rot_var = config["odom_rot"].as<double>();
-  const double odom_trans_var = config["odom_trans"].as<double>();
-  const double lc_rot_var = config["loop_close_rot"].as<double>();
-  const double lc_trans_var = config["loop_close_trans"].as<double>();
-  const bool use_height_bound = config["use_height_bound"].as<bool>();
-  const bool use_height_prior = config["use_height_prior"].as<bool>();
-
-  Eigen::VectorXd odom_vars(6);
-  odom_vars << odom_rot_var, odom_rot_var, odom_rot_var, odom_trans_var, odom_trans_var,
-      odom_trans_var;
-  auto odom_noise = gtsam::noiseModel::Diagonal::Variances(odom_vars);
-
-  Eigen::VectorXd lc_vars(6);
-  lc_vars << lc_rot_var, lc_rot_var, lc_rot_var, lc_trans_var, lc_trans_var,
-      lc_trans_var;
-  auto lc_noise = gtsam::noiseModel::Diagonal::Variances(lc_vars);
-
-  gtsam::NonlinearFactorGraph factors;
-  for (const auto& base_factor : old_factors) {
-    const auto& factor = dynamic_cast<const PoseFactor&>(*base_factor);
-    const int64_t idx1 = gtsam::Symbol(factor.keys()[0]).index();
-    const int64_t idx2 = gtsam::Symbol(factor.keys()[1]).index();
-    if (std::abs(idx1 - idx2) > 1) {
-      LOG(INFO) << "Loop closure: " << gtsam::Symbol(factor.keys()[0]) << " -> "
-                << gtsam::Symbol(factor.keys()[1]);
-      factors.emplace_shared<PoseFactor>(
-          factor.keys()[0], factor.keys()[1], factor.measured(), lc_noise);
-    } else {
-      factors.emplace_shared<PoseFactor>(
-          factor.keys()[0], factor.keys()[1], factor.measured(), odom_noise);
-    }
-  }
-
-  for (const auto& constraint : config["heights"]) {
-    size_t start = constraint["start"].as<size_t>();
-    size_t stop = constraint["stop"].as<size_t>();
-    size_t inc = constraint["inc"].as<size_t>();
-    double lower = constraint["lower"].as<double>();
-    double upper = constraint["upper"].as<double>();
-    double mu = constraint["mu"].as<double>();
-    LOG(INFO) << "Adding height constraint: range(" << start << ", " << stop << ", "
-              << inc << ") -> [" << lower << ", " << upper << "], mu=" << mu;
-    if (use_height_bound) {
-      add_height_bounds(factors, values, start, stop, inc, lower, upper, mu);
-    }
-    if (use_height_prior) {
-      add_height(factors, values, start, stop, inc, (lower + upper) / 2.0, mu * mu);
-    }
-  }
-
-  // TODO(nathan) parse from yaml
   RobustSolverParams params;
   params.setPcmSimple3DParams(config["odom_trans_threshold"].as<double>(),
                               config["odom_rot_threshold"].as<double>(),
@@ -222,35 +335,26 @@ int main(int argc, char* argv[]) {
       config["gnc_cost_tolerance"].as<double>(),
       config["gnc_weight_tolerance"].as<double>());
 
+  gtsam::Values values;
+  gtsam::NonlinearFactorGraph factors, old_factors;
+  if (FLAGS_use_g2o) {
+    load_factors_g2o(old_factors, values);
+  } else {
+    load_factors_pgmo(params, factors, values);
+  }
+
+  remap_covariances(config, old_factors, factors);
+  add_priors_from_config(config, values, factors);
+  add_bounds_from_config(config, values, factors);
+  add_manual_loop_clousres(config, factors);
+
   RobustSolver solver(params);
   solver.forceUpdate(factors, values);
 
   auto opt_values = solver.calculateEstimate();
   std::cout << "Finished!" << '\a' << std::endl;
 
-  gtsam::writeG2o(factors, opt_values, "opt_trajectory.g2o");
-
-  std::string dsg_file = FLAGS_result_dir + "/" + FLAGS_dsg_file;
-  auto timestamps = read_timestamps(dsg_file);
-  if (timestamps.size() != opt_values.size()) {
-    LOG(FATAL) << "timestamps and values disagree: " << timestamps.size()
-               << " != " << opt_values.size();
-  }
-
-  std::ofstream fout("opt_trajectory.csv");
-  fout << "#timestamp_kf,x,y,z,qw,qx,qy,qz" << std::endl;
-
-  for (const auto& kv_pair : opt_values) {
-    const size_t idx = gtsam::Symbol(kv_pair.key).index();
-    fout << timestamps[idx] << ",";
-
-    auto pose = kv_pair.value.cast<gtsam::Pose3>();
-    Eigen::Vector3d t = pose.translation();
-    auto q = pose.rotation().toQuaternion();
-
-    fout << t.x() << "," << t.y() << "," << t.z() << "," << q.w() << "," << q.x() << ","
-         << q.y() << "," << q.z() << std::endl;
-  }
+  output(factors, opt_values);
 
   return 0;
 }
