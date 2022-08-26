32171314b8859420983adba6ebfe9900eb89c785
Nathan Hughes <na26933@mit.edu>
2022-08-26 11:45:39 -0400

    Feature/d455 vio (#132)
    * (wip) try to get a image pipeline together
    
    * (wip) add publisher for rectified camera info
    
    * first pass at kimera-vio for d455
    
    * lower numfeatures and fix transform
    
    * turn on lcd optimization for now
    
    * (wip) start working on getting tf tree correct
    
    * (wip) try transforming the imu to match the expected kimera convention
    
    * add quick utility to transform imu data to new frame
    
    * minor debugging changes to ransac and min depth
    
    * add missing a1 mask and uh2 rgbd debug config
    
    * switch defaults for vio launch file
    
    * (wip) update backend params
    
    * (wip) frontend and lcd param updates
    
    * fix script scraping static tfs and simplify vio launch file
    
    * use kimera functionality to replace ros nodelets
    
    * update uh2 rgbd params to base uhumans2 params
    
    * update sidpac vio config
    
    * actually drop vio from sidpac launch
    
    * drop configs (moved to hydra_vio_configs)
    
    * drop unused utilities and rviz changes
    
    * revert glog level change in hydra_topology
    
    * revert configs back to develop (to keep easy access to feature/online_dsg versions)
    
    Co-authored-by: Nathan Hughes <nathan.h.hughes@gmail.com>

--- hydra_dsg_builder/scripts/copy_static_tfs.py
+++ hydra_dsg_builder/scripts/copy_static_tfs.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # Copyright 2022, Massachusetts Institute of Technology.
 # All Rights Reserved
 #
@@ -32,11 +33,12 @@
 # purposes notwithstanding any copyright notation herein.
 #
 #
-#!/usr/bin/env python
 """Copy static tfs from one uhumans2 bag to a launch file."""
 import rosbag
 import argparse
-import sys
+
+NODE_PREFIX = '    <node pkg="tf2_ros" type="static_transform_publisher"'
+TF_STR = ' args="{x} {y} {z} {qx} {qy} {qz} {qw} {parent} {child}"/>\n\n'
 
 
 def main():
@@ -48,35 +50,30 @@ def main():
     parser.add_argument("output", type=str, help="file to output to")
     args = parser.parse_args()
 
-    tf_message = None
-    with rosbag.Bag(args.bag_file, "r") as bag:
-        for topic, msg, t in bag.read_messages(topics=["/tf_static"]):
-            tf_message = msg
-
-    if tf_message is None:
-        print("Could not find static tf in {}".format(args.bag_file))
-        sys.exit(1)
-
+    tf_idx = 0
     with open(args.output, "w") as fout:
         fout.write("<launch>\n\n")
 
-        for idx, msg in enumerate(tf_message.transforms):
-            fout.write('    <node pkg="tf2_ros" type="static_transform_publisher"')
-            name = "bag_static_tf_{}".format(idx)
-            fout.write(' name="{}"'.format(name))
-            fout.write(
-                ' args="{x} {y} {z} {qx} {qy} {qz} {qw} {parent} {child}"/>\n\n'.format(
-                    x=msg.transform.translation.x,
-                    y=msg.transform.translation.y,
-                    z=msg.transform.translation.z,
-                    qx=msg.transform.rotation.x,
-                    qy=msg.transform.rotation.y,
-                    qz=msg.transform.rotation.z,
-                    qw=msg.transform.rotation.w,
-                    parent=msg.header.frame_id,
-                    child=msg.child_frame_id,
-                )
-            )
+        with rosbag.Bag(args.bag_file, "r") as bag:
+            for topic, tf_msg, t in bag.read_messages(topics=["/tf_static"]):
+                for msg in tf_msg.transforms:
+                    fout.write(NODE_PREFIX)
+                    name = "bag_static_tf_{}".format(tf_idx)
+                    tf_idx += 1
+                    fout.write(' name="{}"'.format(name))
+                    fout.write(
+                        TF_STR.format(
+                            x=msg.transform.translation.x,
+                            y=msg.transform.translation.y,
+                            z=msg.transform.translation.z,
+                            qx=msg.transform.rotation.x,
+                            qy=msg.transform.rotation.y,
+                            qz=msg.transform.rotation.z,
+                            qw=msg.transform.rotation.w,
+                            parent=msg.header.frame_id,
+                            child=msg.child_frame_id,
+                        )
+                    )
 
         fout.write("</launch>\n")
 
