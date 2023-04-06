c8d61c217ae81dc339b1ba00af52a98726881314
Nathan Hughes <nathan.h.hughes@gmail.com>
2023-04-06 20:16:34 +0000

    add script for robot and add optional rosbag record to robot launch file

---
+++ hydra_dsg_builder_ros/scripts/run_robot.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]] ; then
+    echo 'Log path required!!'
+    exit 1
+fi
+
+if [[ -d $1 ]] ; then
+    echo 'Log path '$1' exists!'
+    exit 1
+fi
+
+
+# Make Hydra Folders
+mkdir -p $1
+mkdir -p $1/frontend
+mkdir -p $1/backend
+mkdir -p $1/lcd
+mkdir -p $1/pgmo
+mkdir -p $1/topology
+
+# Make Log Directory
+mkdir -p $1/logs
+
+roslaunch hydra_dsg_builder_ros hydra_robot.launch dsg_path:="$1" glog_to_file:=true glog_dir:="$1/logs" verbosity:=2 record_bag:=true exit_mode:=SERVICE
