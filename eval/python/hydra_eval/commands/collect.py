f56043f2293d9d55bb4afdf186e36f811ebf4e44
Nathan Hughes <nathan.h.hughes@gmail.com>
2025-05-23 13:44:48 +0000

    add ability to skip scenes that are already present

--- eval/python/hydra_eval/commands/collect.py
+++ eval/python/hydra_eval/commands/collect.py
@@ -15,7 +15,8 @@ def cli():
 @cli.command(name="run")
 @click.argument("output_path", type=click.Path())
 @click.argument("experiments", type=click.Path(exists=True), nargs=-1)
-def run(output_path, experiments):
+@click.option("--skip-existing", "-s", is_flag=True)
+def run(output_path, experiments, skip_existing):
     """Collect a new set of Hydra results."""
     with ResultManager(output_path) as manager:
         for experiment_path in experiments:
@@ -23,7 +24,7 @@ def run(output_path, experiments):
             if manager is None:
                 continue
 
-            experiment.run()
+            experiment.run(skip_existing)
 
 
 @cli.command(name="list")
@@ -32,4 +33,19 @@ def list(output_path):
     """Collect a new set of Hydra results."""
     with ResultManager(output_path) as manager:
         for result in manager.results:
+            pprint.pprint(result)
+
+
+@cli.command(name="find")
+@click.argument("output_path", type=click.Path(exists=True))
+@click.argument("experiment_name", type=str)
+@click.argument("trial_name", type=str)
+def find(output_path, experiment_name, trial_name):
+    """Collect a new set of Hydra results."""
+    with ResultManager(output_path) as manager:
+        header = f"Results for {trial_name}:{experiment_name}:"
+        print(header)
+        print("-" * len(header))
+        results = manager.find_trials(experiment_name, trial_name)
+        for result in results:
             pprint.pprint(result)
