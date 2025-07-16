0e1aff48c3d47e437ba16aa3679c4a242a36e7ae
anaveen <aryan_naveen@college.harvard.com>
2025-07-16 03:17:39 -0400

    refactored to support multi-sensor static masks

--- include/hydra/reconstruction/projective_integrator.h
+++ include/hydra/reconstruction/projective_integrator.h
@@ -109,8 +109,6 @@ class ProjectiveIntegrator {
         InterpolatorAdaptive::Config{}};
     //! Semantic integrator configuration (optional)
     config::VirtualConfig<SemanticIntegrator> semantic_integrator;
-    //! Static Mask for Camera
-    std::string static_mask_file_path = "";
   } const config;
 
   struct VoxelMeasurement {
