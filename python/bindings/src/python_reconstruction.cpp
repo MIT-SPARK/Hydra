2d2322d88d11121108add39508c61eb319a0688e
Nathan Hughes <nathan.h.hughes@gmail.com>
2025-07-18 17:51:57 +0000

    (fix) use default layers in scene graph constructor

--- python/bindings/src/python_reconstruction.cpp
+++ python/bindings/src/python_reconstruction.cpp
@@ -65,7 +65,7 @@ struct MeshUpdater {
       : compression(voxel_size / 4.0),
         queue(new ReconstructionModule::OutputQueue()),
         zmq_sender(url, 2) {
-    graph.reset(new DynamicSceneGraph(DynamicSceneGraph::LayerIds{2, 3, 4, 5}));
+    graph.reset(new DynamicSceneGraph());
     graph->setMesh(std::make_shared<Mesh>());
   }
 
