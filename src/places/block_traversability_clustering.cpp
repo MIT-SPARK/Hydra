ea43cd5cb7dfa8bc478cf0b474aacaab7175bf03
lschmid <lschmid@mit.edu>
2025-10-08 13:34:21 -0400

    run precommit

--- src/places/block_traversability_clustering.cpp
+++ src/places/block_traversability_clustering.cpp
@@ -32,6 +32,8 @@
  * Government is authorized to reproduce and distribute reprints for Government
  * purposes notwithstanding any copyright notation herein.
  * -------------------------------------------------------------------------- */
+#include "hydra/places/block_traversability_clustering.h"
+
 #include <config_utilities/config.h>
 #include <config_utilities/validation.h>
 #include <spark_dsg/edge_attributes.h>
@@ -39,7 +41,6 @@
 
 #include <queue>
 
-#include "hydra/places/block_traversability_clustering.h"
 #include "hydra/utils/timing_utilities.h"
 
 namespace hydra::places {
