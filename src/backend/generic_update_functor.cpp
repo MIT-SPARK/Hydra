fa97f31b6c7ff18a149e615fad63051d94f2317d
Nathan Hughes <nathan.h.hughes@gmail.com>
2025-11-02 08:23:48 -0500

    Feature/deformation functor (#115)
    * add new generic update functor
    
    * use new deformation api
    
    * try to use attribute transform
    
    * fix khronos object timestamps and remove double transform
    
    * (wip) debug log changes
    
    * fix double transformation and add unit tests
    
    * only transform position for now
    
    * fix formatting

---
+++ src/backend/generic_update_functor.cpp
@@ -0,0 +1,125 @@
+/* -----------------------------------------------------------------------------
+ * Copyright 2022 Massachusetts Institute of Technology.
+ * All Rights Reserved
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Research was sponsored by the United States Air Force Research Laboratory and
+ * the United States Air Force Artificial Intelligence Accelerator and was
+ * accomplished under Cooperative Agreement Number FA8750-19-2-1000. The views
+ * and conclusions contained in this document are those of the authors and should
+ * not be interpreted as representing the official policies, either expressed or
+ * implied, of the United States Air Force or the U.S. Government. The U.S.
+ * Government is authorized to reproduce and distribute reprints for Government
+ * purposes notwithstanding any copyright notation herein.
+ * -------------------------------------------------------------------------- */
+#include "hydra/backend/generic_update_functor.h"
+
+#include <config_utilities/config.h>
+#include <config_utilities/validation.h>
+#include <glog/logging.h>
+
+#include "hydra/utils/timing_utilities.h"
+
+namespace hydra {
+namespace {
+
+static const auto registration =
+    config::RegistrationWithConfig<UpdateFunctor,
+                                   GenericUpdateFunctor,
+                                   GenericUpdateFunctor::Config>(
+        "GenericUpdateFunctor");
+
+}
+
+using timing::ScopedTimer;
+
+void declare_config(GenericUpdateFunctor::Config& config) {
+  using namespace config;
+  name("GenericUpdateFunctor::Config");
+  field(config.layer, "layer");
+  field(config.deformation_interpolator, "deformation_interpolator");
+  field(config.enable_merging, "enable_merging");
+  config.matcher.setOptional();
+  field(config.matcher, "node_matcher");
+  field(config.merge_proposer, "merge_proposer");
+  checkCondition(!config.layer.empty(), "layer must be non-empty!");
+}
+
+GenericUpdateFunctor::GenericUpdateFunctor(const Config& config)
+    : config(config::checkValid(config)),
+      node_matcher(config.matcher.create()),
+      merge_proposer(config.merge_proposer),
+      deformation_interpolator(config.deformation_interpolator) {}
+
+UpdateFunctor::Hooks GenericUpdateFunctor::hooks() const {
+  auto my_hooks = UpdateFunctor::hooks();
+  if (config.enable_merging && node_matcher) {
+    my_hooks.find_merges = [this](const auto& graph, const auto& info) {
+      return findMerges(graph, info);
+    };
+  }
+
+  return my_hooks;
+}
+
+void GenericUpdateFunctor::call(const DynamicSceneGraph& unmerged,
+                                SharedDsgInfo& dsg,
+                                const UpdateInfo::ConstPtr& info) const {
+  ScopedTimer spin_timer("backend/update_" + config.layer, info->timestamp_ns);
+  if (!unmerged.hasLayer(config.layer)) {
+    return;
+  }
+
+  const auto new_loopclosure = info->loop_closure_detected;
+  const auto& layer = unmerged.getLayer(config.layer);
+  active_tracker.clear();  // reset from previous pass
+  const auto view = new_loopclosure ? LayerView(layer) : active_tracker.view(layer);
+  deformation_interpolator.interpolateNodePositions(unmerged, *dsg.graph, info, view);
+  MLOG(1) << "[Hydra Backend] " << config.layer << " update: " << layer.numNodes()
+          << " nodes";
+}
+
+MergeList GenericUpdateFunctor::findMerges(const DynamicSceneGraph& graph,
+                                           const UpdateInfo::ConstPtr& info) const {
+  if (!node_matcher) {
+    LOG(WARNING) << "No node matcher!";
+    return {};
+  }
+
+  const auto new_lcd = info->loop_closure_detected;
+  const auto& layer = graph.getLayer(config.layer);
+  // freeze layer view to avoid messing with tracker
+  const auto view = new_lcd ? LayerView(layer) : active_tracker.view(layer, true);
+
+  MergeList proposals;
+  merge_proposer.findMerges(
+      layer,
+      view,
+      [this](const SceneGraphNode& lhs, const SceneGraphNode& rhs) {
+        return node_matcher->match(lhs.attributes(), rhs.attributes());
+      },
+      proposals);
+  return proposals;
+}
+
+}  // namespace hydra
