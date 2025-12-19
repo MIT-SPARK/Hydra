ea290dd8797ed0c82ffa21e7464f7cb5e4f49534
Nathan Hughes <nathan.h.hughes@gmail.com>
2025-12-19 19:42:52 +0000

    clean up command line interface

--- eval/python/hydra_eval/commands/timing.py
+++ eval/python/hydra_eval/commands/timing.py
@@ -15,9 +15,16 @@ def cli():
 
 @cli.command(name="show")
 @click.argument("result_path", type=click.Path(exists=True))
-@click.option("-k", "--key", "keys", multiple=True, default=None)
+@click.option(
+    "-k",
+    "--key",
+    "keys",
+    multiple=True,
+    default=None,
+    help="optional regex filters to select relevant timers",
+)
 def show(result_path, keys):
-    """Print timing information for a single set of Hydra results."""
+    """Print timing distribution from Hydra's output at RESULT_PATH."""
     result_path = pathlib.Path(result_path).expanduser().absolute()
     durations = timing.collect_timing_info(result_path)
     if len(durations) == 0:
@@ -33,34 +40,67 @@ def show(result_path, keys):
 
 @cli.command(name="plot")
 @click.argument("result_path", type=click.Path(exists=True))
-@click.option("-t", "--show-trends", is_flag=True, default=False)
-@click.option("-k", "--key", "keys", multiple=True, default=("frontend.*", "backend.*"))
-def plot(result_path, show_trends, keys):
-    """Plot timing information for a single set of Hydra results."""
+@click.option(
+    "-k",
+    "--key",
+    "keys",
+    multiple=True,
+    default=("frontend.*", "backend.*"),
+    help="optional regex filters to select timers for each subplot",
+)
+def plot(result_path, keys):
+    """Plot timing distribution from Hydra's output at RESULT_PATH."""
     result_path = pathlib.Path(result_path).expanduser().absolute()
     durations = timing.collect_timing_info(result_path)
     if len(durations) == 0:
         click.secho(f"Result path {result_path} does not contain timing info", fg="red")
         sys.exit(1)
 
-    if show_trends:
-        timing.plot_trends(durations, keys=keys)
-    else:
-        timing.plot_durations(durations, keys=keys)
+    timing.plot_durations(durations, keys=keys)
+
+
+@cli.command(name="timeline")
+@click.argument("result_path", type=click.Path(exists=True))
+@click.option(
+    "-k",
+    "--key",
+    "keys",
+    multiple=True,
+    default=("frontend.*", "backend.*"),
+    help="optional regex filters to select timers to plot",
+)
+def timeline(result_path, keys):
+    """Plot timing information against time from Hydra's output at RESULT_PATH."""
+    result_path = pathlib.Path(result_path).expanduser().absolute()
+    durations = timing.collect_timing_info(result_path)
+    if len(durations) == 0:
+        click.secho(f"Result path {result_path} does not contain timing info", fg="red")
+        sys.exit(1)
+
+    timing.plot_trends(durations, keys=keys)
 
 
 @cli.command(name="compare")
 @click.argument("results", nargs=-1, type=click.Path(exists=True))
 @click.option(
-    "-k", "--key", "keys", multiple=True, default=("frontend/spin", "backend/spin")
+    "-k",
+    "--key",
+    "keys",
+    multiple=True,
+    default=("frontend/spin", "backend/spin"),
+    help="timer names to use for comparison",
 )
-@click.option("--use-bars", is_flag=True, default=False)
-def compare(results, keys, use_bars):
-    """Plot timing comparison between different runs of Hydra."""
+@click.option(
+    "--use-bar",
+    is_flag=True,
+    default=False,
+    help="use a bar plot instead of a boxen plot",
+)
+def compare(results, keys, use_bar):
+    """Plot timing comparison between different runs of Hydra from RESULTS."""
     if len(results) == 0:
         return
 
     paths = [pathlib.Path(x).expanduser().absolute() for x in results]
     results = {path.stem: timing.collect_timing_info(path) for path in paths}
-
-    timing.plot_comparison(results, keys, use_bars=use_bars)
+    timing.plot_comparison(results, keys, use_bars=use_bar)
