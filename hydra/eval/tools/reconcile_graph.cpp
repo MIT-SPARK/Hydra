ed74e119beefe91e08d9de9f3af65016cb89b5bb
Nathan Hughes <nathan.h.hughes@gmail.com>
2026-08-31 12:20:31 +0000

    fix active flag and print info

--- hydra/eval/tools/reconcile_graph.cpp
+++ hydra/eval/tools/reconcile_graph.cpp
@@ -34,9 +34,13 @@
  * -------------------------------------------------------------------------- */
 #include <config_utilities/config.h>
 #include <config_utilities/parsing/context.h>
+#include <config_utilities/printing.h>
 #include <config_utilities/validation.h>
+#include <gflags/gflags.h>
+#include <glog/logging.h>
 #include <hydra/backend/merge_tracker.h>
 #include <hydra/backend/update_functions.h>
+#include <spark_dsg/printing.h>
 #include <spark_dsg/scene_graph.h>
 
 #include <CLI/CLI.hpp>
@@ -50,6 +54,8 @@ struct AppArgs {
   std::filesystem::path scene_graph;
   std::string config_ns = "backend";
   std::filesystem::path output;
+  int log_level = 0;
+  int verbosity = 0;
 };
 
 void AppArgs::add_to_app(CLI::App& app) {
@@ -57,8 +63,10 @@ void AppArgs::add_to_app(CLI::App& app) {
       ->required()
       ->check(CLI::ExistingFile)
       ->description("Graph to reconcile");
-  app.add_option("-n,--namespace", config_ns)->description("Config namespace to use");
+  app.add_option("--namespace", config_ns)->description("Config namespace to use");
   app.add_option("-o,--output", output)->description("File to output to");
+  app.add_option("-l,--log-level", log_level)->description("GLOG log level");
+  app.add_option("--verbosity", verbosity)->description("GLOG verbosity");
 }
 
 struct Reconciler {
@@ -77,7 +85,16 @@ struct Reconciler {
   std::vector<std::pair<std::string, std::unique_ptr<UpdateFunctor>>> functors;
 };
 
+void declare_config(Reconciler::Config& config) {
+  using namespace config;
+  name("Reconciler::Config");
+  field(config.update_functors, "update_functors");
+  field(config.exhaustive_functors, "exhaustive_functors");
+}
+
 Reconciler::Reconciler(const Config& config) : config(config::checkValid(config)) {
+  std::cout << "Creating reconciler with config:\n"
+            << config::toString(config) << std::endl;
   for (const auto& [name, functor] : config.update_functors) {
     functors.emplace_back(name, functor.create());
   }
@@ -91,6 +108,7 @@ auto Reconciler::reconcile(Graph& graph) const -> Graph::Ptr {
   target.graph = graph.clone();
 
   auto info = std::make_shared<UpdateInfo>();
+  info->loop_closure_detected = true;
   GroupedMergeTracker merge_tracker;
   std::list<UpdateFunctor::Hooks::CleanupFunc> cleanup_hooks;
   for (const auto& [name, functor] : functors) {
@@ -98,6 +116,7 @@ auto Reconciler::reconcile(Graph& graph) const -> Graph::Ptr {
       continue;
     }
 
+    merge_tracker.initializeTracker(name);
     const auto hooks = functor->hooks();
     if (hooks.cleanup) {
       cleanup_hooks.push_back(hooks.cleanup);
@@ -108,14 +127,19 @@ auto Reconciler::reconcile(Graph& graph) const -> Graph::Ptr {
       auto& tracker = merge_tracker.getMergeGroup(name);
       auto merges = hooks.find_merges(graph, info);
       auto applied = tracker.applyMerges(graph, merges, target, hooks.merge);
+      VLOG(1) << "pass 0: " << merges.size() << " merges (applied " << applied << ")";
       if (!exhaustive_names.count(name)) {
         continue;
       }
 
       applied = 0;
+      size_t iter = 0;
       do {
         merges = hooks.find_merges(*target.graph, info);
         applied = tracker.applyMerges(graph, merges, target, hooks.merge);
+        VLOG(1) << "pass " << iter << ": " << merges.size() << " merges (applied "
+                << applied << ")";
+        ++iter;
       } while (applied > 0);
     }
 
@@ -127,11 +151,32 @@ auto Reconciler::reconcile(Graph& graph) const -> Graph::Ptr {
   return target.graph;
 }
 
-void declare_config(Reconciler::Config& config) {
-  using namespace config;
-  name("Reconciler::Config");
-  field(config.update_functors, "update_functors");
-  field(config.exhaustive_functors, "exhaustive_functors");
+void printCounts(spark_dsg::SceneGraph& graph) {
+  for (const auto& [layer_id, layer] : graph.layers()) {
+    std::cout << "Layer " << layer->id << ": " << layer->numNodes() << std::endl;
+  }
+
+  for (const auto& [layer_id, partitions] : graph.layer_partitions()) {
+    for (const auto& [key, layer] : partitions) {
+      std::cout << "Layer " << layer->id << ": " << layer->numNodes() << std::endl;
+    }
+  }
+}
+
+void resetActive(spark_dsg::SceneGraph& graph) {
+  for (const auto& [layer_id, layer] : graph.layers()) {
+    for (const auto& [node_id, node] : layer->nodes()) {
+      node->attributes().is_active = false;
+    }
+  }
+
+  for (const auto& [layer_id, partitions] : graph.layer_partitions()) {
+    for (const auto& [key, layer] : partitions) {
+      for (const auto& [node_id, node] : layer->nodes()) {
+        node->attributes().is_active = false;
+      }
+    }
+  }
 }
 
 }  // namespace hydra
@@ -154,6 +199,11 @@ int main(int argc, char** argv) {
     return app.exit(e);
   }
 
+  FLAGS_minloglevel = 0;
+  FLAGS_logtostderr = 1;
+  FLAGS_colorlogtostderr = 1;
+  google::InitGoogleLogging(argv[0]);
+
   auto output = args.output;
   if (output.empty()) {
     const auto filename = args.scene_graph.stem().string() + "_reconciled.json";
@@ -163,8 +213,16 @@ int main(int argc, char** argv) {
   const auto config = config::fromContext<hydra::Reconciler::Config>(args.config_ns);
   const auto reconciler = hydra::Reconciler(config);
 
+  std::cout << "Loading graph from " << args.scene_graph << "..." << std::endl;
   auto graph = spark_dsg::SceneGraph::load(args.scene_graph);
+  hydra::resetActive(*graph);
+  std::cout << "Before:\n=======" << std::endl;
+  hydra::printCounts(*graph);
+  std::cout << "Reconciling graph..." << std::endl;
   auto new_graph = reconciler.reconcile(*graph);
-  new_graph->save(args.output, true);
+  std::cout << "After:\n======" << std::endl;
+  hydra::printCounts(*new_graph);
+  std::cout << "Saving graph to " << output << "..." << std::endl;
+  new_graph->save(output, true);
   return 0;
 }
